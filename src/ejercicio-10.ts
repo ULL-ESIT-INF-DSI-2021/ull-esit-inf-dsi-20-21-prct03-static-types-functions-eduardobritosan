bd3afae109845bc7ff88b27f98fa4e1c3b591b8c
Eduardo Brito <alu0100783315@ull.edu.es>
2021-03-06 19:26:03 +0000

    Deployment commit

--- src/ejercicio-10.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Revisa la validez del nombre de usuario
- * @param username El usuario a revisar
- * @returns true si la cadena es valida y false si no lo es
- */
-/* eslint-disable max-len */
-export function isValidUsername(username: string): boolean {
-  return /^(?!_)((?=.*[a-z]){1,2}(?=.*[0-9]){1,2}(?=.*[A-Z]){1,2}(?=.*[$\-_]){1,2})(?!.*([A-Za-z0-9_$\-])\2{2})([a-z0-9A-Z$\-_]){4,30}([^_])$/g.test(username);
-}
